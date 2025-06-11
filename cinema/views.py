b73158cfe0723dcd02c291eee238a444e5c87a8a
Alona Tsapenko <alena.tsapenko7@gmail.com>
2025-06-11 21:13:52 +0200

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -147,4 +147,3 @@ class MovieSessionListSerializerWithTicketsAvailable(MovieSessionListSerializer)
         total_capacity = obj.cinema_hall.capacity
         taken_count = obj.tickets.count()
         return total_capacity - taken_count
-    
